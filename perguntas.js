a60134cd7e14fc2aed6342f29efed5dc00fd2fc9
Emanuelly <siani.emanuelly@escola.pr.gov.br>
2025-10-01 15:21:24 -0300

    01/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'quem é feia?',
+    'Vitória'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quem é linda?',
+    'manu'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'quem é o mais bundudo?',
+    'João Rafael'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'quem namora comigo?',
+    'conrad gostoso'
 )
